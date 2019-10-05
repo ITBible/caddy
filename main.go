5fe90d8fba638882fecf4b498743f5c022a23652
Connor Strandt <connor.strandt@gmail.com>
2019-10-05 11:32:09 -0500

    Added Caddy-Git Plugin
    The active plugin is a forked workaround to path errors with the actual plugin.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/caddyserver/caddy/caddy/caddymain"
 	// Caddy plugins
 	_ "github.com/caddyserver/dnsproviders/digitalocean"
+	//_ "github.com/abiosoft/caddy-git"
+    _ "github.com/awoodbeck/caddy-git"
 )
 
 func main() {
