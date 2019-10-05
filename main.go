de830b396789bf64a5eee558a483c62e703b0a7e
Connor Strandt <connor.strandt@gmail.com>
2019-10-05 11:38:55 -0500

    Fixed spacing.

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	// Caddy plugins
 	_ "github.com/caddyserver/dnsproviders/digitalocean"
 	//_ "github.com/abiosoft/caddy-git"
-    _ "github.com/awoodbeck/caddy-git"
+	_ "github.com/awoodbeck/caddy-git"
 )
 
 func main() {
